20558195b7068fc294074b11ce1c809b09209536
João Victor <va60234@gmail.com>
2026-04-01 20:01:16 -0300

    Add files via upload

---
+++ Lista 002/CalcularDesconto.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CalcularDesconto {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o valor total da sua compra R$ ");
+        float valorDaCompra = scan.nextFloat();
+        scan = new Scanner(System.in);
+
+        System.out.println("Você se encaixa em qual categoria de cliente? (Novo ou Fidelidade)");
+        String tipoCliente = scan.nextLine();
+
+       
+        if (valorDaCompra > 100 || tipoCliente.equalsIgnoreCase("Fidelidade")) {
+            System.out.println(String.format("Parabéns! Você ganhou um desconto de R$ %.2f" , valorDaCompra * 0.15));
+        } 
+
+        else {
+            System.out.println(String.format("O total a pagar é de R$ %.2f" , valorDaCompra));
+        }
+
+        scan.close();
+    }
+
+}
