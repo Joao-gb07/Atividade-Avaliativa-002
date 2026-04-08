2bd029aeb9a97a0f3702580afe00123a309595b8
João Victor <va60234@gmail.com>
2026-04-08 16:37:40 -0300

    Organizar em ordem Cresente

---
+++ Lista 002/VerificarOrdemCresente.java
@@ -0,0 +1,38 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class VerificarOrdemCresente {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int num1 = scan.nextInt();
+
+        System.out.println("Digite um número: ");
+        int num2 = scan.nextInt();
+
+        System.out.println("Digite um número: ");
+        int num3 = scan.nextInt();
+
+        System.out.println("Os números digitados fora de ordem foram: " + num1 + "-" + num2 + "-" + num3);
+
+        int maiorNumero;
+        int segundoMaior;
+        int terceiroMaior;
+
+        if (num1 > num2 && num1 > num3) {
+            maiorNumero = num1;
+        }
+        else if (num2 > num1 && num2 > num3) {
+            segundoMaior = num2;
+        }
+        else {
+            terceiroMaior = num3;
+        }
+        System.out.println("Os números digitados em ordem cresente ficam assim: " + terceiroMaior  + segundoMaior  + maiorNumero);
+
+
+
+    }
+    
+}
